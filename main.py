bcb04c26d552db88dbfd612c6dbfa0a221d9c707
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2020-11-26 20:24:32 +0700

    add qid 2 to 5

--- main.py
+++ main.py
@@ -1,54 +1,20 @@
 # Module Imports
 from prettytable import PrettyTable
+
 from pprint import pprint
+from time import sleep
 
 from ui import print_menu
 from dbconn import get_db_cursor
+from queries import queries
 
 cur = get_db_cursor()
 
-queries = [
-    {},
-    {
-        "sql":"""
-            SELECT de.dept_no, dn.dept_name, COUNT(de.emp_no) as count_employee
-            FROM employees e
-                JOIN dept_emp de ON e.emp_no = de.emp_no
-                JOIN departments dn ON de.dept_no = dn.dept_no
-            WHERE de.from_date >= '1999-01-01' AND de.from_date <= '2000-01-01' 
-            GROUP BY dn.dept_no
-        """,
-        "fields": ["dept_no", "dept_name", "count_employee"],
-    },
-    {
-        "sql": "TODO",
-        "fields": ["TODO"],
-    },
-    {
-        "sql": "TODO",
-        "fields": ["TODO"],
-    },
-    {
-        "sql": "TODO",
-        "fields": ["TODO"],
-    },
-    {
-        "sql": "TODO",
-        "fields": ["TODO"],
-    },
-    {
-        "sql": "TODO",
-        "fields": ["TODO"],
-    },
-    {
-        "sql": "TODO",
-        "fields": ["TODO"],
-    },
-]
-
-while True:
+rep = True
+while rep:
     print_menu()
     query_id = int(input("Query ID: "))
+    print("Result:")
 
     x = PrettyTable()
     if query_id >= 1 and query_id < len(queries):
@@ -60,5 +26,8 @@ while True:
             for c in cur:
                 x.add_row(c)
             print(x)
-
     
+    user_rep = input("Repeat? (y / n): ")
+    while user_rep != "y" and user_rep != "n":
+        user_rep = input("Repeat? (y / n): ")
+    rep = (user_rep == "y")
